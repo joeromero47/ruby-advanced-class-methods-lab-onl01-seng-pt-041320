955a8010d3b7b0c82f2c682dc7a30a62fcf26928
Joseph Romero <joeromero47@gmail.com>
2020-05-07 20:53:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,9 @@ class Song
     song.save
     song
   end
-  def self.new_by_name(title)
+  def self.new_by_name(name)
     song = self.new
-    song.name = title
+    song.name = name
     song
   end
 end
